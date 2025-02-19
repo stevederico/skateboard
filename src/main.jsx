e3a5bce6901620c2812ea36e819842ff220525b8
Steve Derico <steve@bixbyapps.com>
2025-02-19 12:33:08 -0800

    added icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import './assets/styles.css'
+import "./assets/bootstrap-icons/font/bootstrap-icons.css" 
 import DemoApp from './components/DemoApp.jsx'
 import NotFound from './components/NotFound.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
     <Router>
       <Routes>
         <Route path="/" element={<DemoApp />} />
