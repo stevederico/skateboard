cc11f748026029863aa2d3df93f3eb2d04c12c96
Steve Derico <steve@bixbyapps.com>
2025-02-19 18:49:25 -0800

    centered settings view

--- src/components/SettingsView.jsx
+++ src/components/SettingsView.jsx
@@ -10,18 +10,18 @@ export default function SettingsView() {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="text-xl font-semibold my-3 mx-3">SettingsView</div>
-      <div className="rounded px-3 border cursor-pointer m-1 flex-none self-center" onClick={toggleDarkMode}>
-        Toggle Dark Mode
+    <>
+      <div className="text-xl font-semibold mb-3">SettingsView</div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="rounded px-3 border cursor-pointer m-1" onClick={toggleDarkMode}> Toggle Dark Mode</div>
+        <div className="rounded px-3 px-2 border cursor-pointer m-1" onClick={() => {
+          document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+          navigate('/');
+        }}> Sign Out</div>
+        <div className="text-sm my-2">{state.constants.version}</div>
       </div>
-      <div className="text-sm my-2 mx-3">Version: {state.constants.version}</div>
-      <div className="rounded px-3 px-2 border cursor-pointer m-1 flex-none self-center" onClick={() => {
-        document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-        console.log("SIGNED OUT");
-        navigate('/');
-      }}> Sign Out</div>
-    </div>
+    </>
+
   );
 }
 
