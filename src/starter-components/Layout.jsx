9b2584e4c0a06de07bf0d44c6b3aa887f3637dbc
Steve Derico <steve@bixbyapps.com>
2025-03-01 15:58:52 -0800

    added Shadcn/ui Sidebar

--- src/starter-components/Layout.jsx
+++ src/starter-components/Layout.jsx
@@ -1,24 +1,28 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar.jsx'
 import TabBar from './TabBar.jsx'
+import { SidebarProvider, SidebarTrigger } from "@/shadcn/ui/components/ui/sidebar"
+import { AppSidebar } from "@/shadcn/ui/components/ui/app-sidebar"
 
-export default function Layout() {
+
+export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-light-back dark:bg-dark-back text-light-text dark:text-dark-text">
       <div className="fixed inset-0 flex overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 relative overflow-y-auto scrollbar-hide">
-          <div className="container mx-auto px-4 py-8 mb-16 md:mb-0">
-            <Outlet />
-          </div>
-        </main>
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="flex-1 relative overflow-y-auto scrollbar-hide">
+            <div className="container mx-auto px-4 py-8 mb-16 md:mb-0">
+              <Outlet />
+            </div>
+          </main>
+        </SidebarProvider>
       </div>
       <TabBar className="md:hidden" />
     </div>
-  );
+  )
 }
 
 
 
 
-  
+
