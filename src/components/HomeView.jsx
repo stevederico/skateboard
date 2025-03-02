b487792ee9382811f976697d97a8ab64eb9631bf
Steve Derico <steve@bixbyapps.com>
2025-03-02 09:35:26 -0800

    added basic headers to homeview and other

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,9 +1,9 @@
-export default function OtherView() {
+export default function HomeView() {
   return (
     <>
       <div className="flex border-b w-full">
         <div className="m-3 p-2 text-sm hover:bg-accent hover:text-accent-foreground rounded cursor-pointer font-medium">
-          Other
+          Home
         </div>
       </div>
     </>
--- src/components/MainView.jsx
+++
@@ -1,9 +0,0 @@
-export default function MainView() {
-  return (
-    <>
-      <div className="flex border-b w-full"></div>
-      <div className="text-xl font-semibold mb-3">Main</div>
-    </>
-  )
-}
-
