4338d521dba475106ae8014c3e28a899d253fce4
Steve Derico <steve@bixbyapps.com>
2025-03-02 13:49:43 -0800

    sign in and up

--- src/starter-components/SignInView.jsx
+++ src/starter-components/SignInView.jsx
@@ -1,9 +1,25 @@
+import { cn } from "@/shadcn/ui/lib/utils"
+import { Button } from "@/shadcn/ui/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/shadcn/ui/components/ui/card"
+import { Input } from "@/shadcn/ui/components/ui/input"
+import { Label } from "@/shadcn/ui/components/ui/label"
+import { DynamicIcon } from "lucide-react/dynamic";
+
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getState } from '@/context.jsx';
 import constants from "@/constants.json";
 
-export default function SignInView() {
+export default function LoginForm({
+  className,
+  ...props
+}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -12,8 +28,7 @@ export default function SignInView() {
 
   async function signInClicked() {
     try {
-      let uri = `${state.constants.backendURL}/signin`
-      console.log("URI: ", uri)
+      let uri = `${constants.backendURL}/signin`
       const response = await fetch(uri, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -40,52 +55,55 @@ export default function SignInView() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white">
-        <div className="flex flex-row items-center justify-center w-full  mb-6">
-          <div className="w-5 md:w-12 mr-0">
-            <img src={"/icons/icon.svg"} />
+    (<div className={cn("flex flex-col gap-6 p-4 max-w-[50%] mx-auto", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <div className="flex flex-row items-center m-2 mx-auto">
+            <div className="bg-app dark:border rounded-lg flex aspect-square size-10 items-center justify-center">
+              <DynamicIcon name={constants.appIcon} size={24} color="white" strokeWidth={2} />
+            </div>
+            <div className="font-semibold ml-2 text-4xl">{constants.appName}</div>
           </div>
-          <div className="text-lg md:text-xl font-semibold ">{constants.appName}</div>
-        </div>
-        {errorMessage !== '' && (
-          <div className="bg-red-200 text-red-500 text-center font-semibold border-2 border-red-500">
-            {errorMessage}
-          </div>
-        )}
-        <div className="space-y-4">
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full px-3 py-2 border rounded-md"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              setErrorMessage('');
-            }}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full px-3 py-2 border rounded-md"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            onClick={signInClicked}
-            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600"
-          >
-            Sign In
-          </button>
-          <button
-            onClick={signUpClicked}
-            className="w-full px-4 py-2 font-bold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300"
-          >
-            Create Account
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+          {errorMessage !== '' && (
 
+            <div className="bg-red-200 text-red-500 text-center font-semibold border-2 border-red-500">
+              {errorMessage}
+            </div>
+
+          )}
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setErrorMessage('');
+                  }} />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button onClick={(e) => { e.preventDefault(); signInClicked() }} className="w-full cursor-pointer">
+                  Sign In
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <span onClick={signUpClicked} className="underline underline-offset-4 cursor-pointer">
+                Sign Up
+              </span>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>)
+  );
 }
--- src/starter-components/SignUpView.jsx
+++ src/starter-components/SignUpView.jsx
@@ -1,19 +1,39 @@
+import { cn } from "@/shadcn/ui/lib/utils"
+import { Button } from "@/shadcn/ui/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/shadcn/ui/components/ui/card"
+import { Input } from "@/shadcn/ui/components/ui/input"
+import { Label } from "@/shadcn/ui/components/ui/label"
+import { DynamicIcon } from "lucide-react/dynamic";
+
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getState } from '@/context.jsx';
 import constants from "@/constants.json";
 
-export default function SignUpView() {
+export default function LoginForm({
+  className,
+  ...props
+}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const { state } = getState();
   const [errorMessage, setErrorMessage] = useState('')
 
+  async function signInClicked() {
+    navigate('/signin');
+  }
+
   async function signUpClicked() {
     try {
-      console.log(`${state.constants.backendURL}/signup`);
-      const response = await fetch(`${state.constants.backendURL}/signup`, {
+      console.log(`${constants.backendURL}/signup`);
+      const response = await fetch(`${constants.backendURL}/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
@@ -35,38 +55,55 @@ export default function SignUpView() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white">
-      <div className="flex flex-row items-center justify-center w-full  mb-6">
-          <div className="w-5 md:w-12 mr-0">
-            <img src={"/icons/icon.svg"} />
+    (<div className={cn("flex flex-col gap-6 p-4 max-w-[50%] mx-auto", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <div className="flex flex-row items-center m-2 mx-auto">
+            <div className="bg-app dark:border rounded-lg flex aspect-square size-10 items-center justify-center">
+              <DynamicIcon name={constants.appIcon} size={24} color="white" strokeWidth={2} />
+            </div>
+            <div className="font-semibold ml-2 text-4xl">{constants.appName}</div>
           </div>
-          <div className="text-lg md:text-xl font-semibold ">{constants.appName}</div>
-        </div>
-        {errorMessage != '' && <div className="bg-red-200 text-red-500 text-center font-semibold border-2 border-red-500">{errorMessage}</div>}
-        <div className="space-y-4">
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full px-3 py-2 border rounded-md"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full px-3 py-2 border rounded-md"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            onClick={signUpClicked}
-            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600"
-          >
-            Sign Up
-          </button>
-        </div>
-      </div>
-    </div>
+          {errorMessage !== '' && (
+
+            <div className="bg-red-200 text-red-500 text-center font-semibold border-2 border-red-500">
+              {errorMessage}
+            </div>
+
+          )}
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setErrorMessage('');
+                  }} />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button onClick={(e) => { e.preventDefault(); signUpClicked() }} className="w-full cursor-pointer">
+                  Sign Up
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <span onClick={(e)=>{e.preventDefault(); signInClicked()}} className="underline underline-offset-4 cursor-pointer">
+                Sign In
+              </span>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>)
   );
 }
