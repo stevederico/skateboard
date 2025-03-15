84fb81a9b24072df02856d3e799015478fed4a3c
Steve Derico <steve@bixbyapps.com>
2025-03-15 09:15:44 -0700

    added headers

--- src/components/OtherView.jsx
+++ src/components/OtherView.jsx
@@ -1,11 +1,12 @@
+import Header from '@stevederico/skateboard-ui/Header';
+
 export default function OtherView() {
   return (
     <>
-      <nav className="flex border-b w-full">
-        <h1 className="m-3 p-2 text-xl hover:bg-accent hover:text-accent-foreground rounded cursor-pointer font-medium">
-          Other
-        </h1>
-      </nav>
+      <Header
+        buttonClass=""
+        title={"Other"}
+      ></Header>
     </>
   )
 }
