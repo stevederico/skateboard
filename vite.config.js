b16a2ef48d316415b4deba505329dcfbfa9c638b
Steve Derico <steve@bixbyapps.com>
2025-08-06 22:37:20 -0700

    0.2.1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
@@ -19,11 +20,26 @@ const customLoggerPlugin = () => {
   };
 };
 
+const htmlReplacePlugin = () => {
+  return {
+    name: 'html-replace',
+    transformIndexHtml(html) {
+      const constants = JSON.parse(readFileSync('src/constants.json', 'utf8'));
+      
+      return html
+        .replace(/{{APP_NAME}}/g, constants.appName)
+        .replace(/{{TAGLINE}}/g, constants.tagline)
+        .replace(/{{COMPANY_WEBSITE}}/g, constants.companyWebsite);
+    }
+  };
+};
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    customLoggerPlugin()
+    customLoggerPlugin(),
+    htmlReplacePlugin()
   ],
   esbuild: {
     drop: ['console', 'debugger']
